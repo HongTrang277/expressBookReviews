60d44485915a28fa90462f8ad8c6455d811277e5
HongTrang277 <ihongtrang277@gmail.com>
2025-08-11 09:33:32 -0400

    feat: method get

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -31,20 +31,53 @@ public_users.get('/isbn/:isbn',function (req, res) {
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const author = req.params.author;
+    const matchingBooks= [];
+    for( const isbn in books){
+        if(books[isbn].author === author){
+            matchingBooks.push({
+                isbn: isbn,
+                title: books[isbn].title,
+                reviews: books[isbn].reviews
+            });
+        }
+    }
+    if(matchingBooks.length >0){
+        return res.status(200).json(matchingBooks);
+    } else {
+        return res.status(404).json({massage: "Không tìm thấy tác giả"})
+    }
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const title = req.params.title;
+    const matchingTitles= [];
+    for( const isbn in books){
+        if(books[isbn].title === title){
+            matchingTitles.push({
+                isbn: isbn,
+                title: books[isbn].title,
+                reviews: books[isbn].reviews
+            });
+        }
+    }
+    if(matchingTitles.length >0){
+        return res.status(200).json(matchingTitles);
+    } else {
+        return res.status(404).json({massage: "Không tìm thấy sách có tiêu đề"})
+    }
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn;
+    const reviewBooks = books[isbn].reviews;
+    if (reviewBooks) {
+        return res.status(200).json(reviewBooks);
+    } else {
+        return res.status(404).json(reviewBooks);
+    }
 });
 
 module.exports.general = public_users;
